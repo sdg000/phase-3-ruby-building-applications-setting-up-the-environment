ad2680d6df2bac0047ab0f3b9c0d5b516270fec1
Seth David Gyimah <esec26@gmail.com>
2022-08-30 00:10:19 +0000

    setting up environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,9 @@
-# Require statements should go here.
+# THIS FILE requires /imports all relative paths
+
+require_relative "../lib/garden"
+require_relative "../lib/plant"
+
+#THIS FILE WILL THEN BE REQUIRED BY RUN FILE
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
